24dda5c6d0c97d77a6150a24b22837ba581d7449
LeeGwonSeon <verygoody2k@hotmail.com>
2025-06-10 11:52:56 +0900

    react_start-20250610

--- explore-canada/src/App.js
+++ explore-canada/src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Destinations from "./components/Destinations";
-import Tips from "./components/Tips";
-import Footer from "./components/Footer";
-
-import './App.scss';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return(
-    <>
-      <Header />
-      <Hero />
-      <Destinations />
-      <Tips />
-      <Footer />
-    </>
-  )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
