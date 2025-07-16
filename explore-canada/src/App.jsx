6879cd31bff116f1f34691d6be6ea018187ef28c
LeeGwonSeon <verygoody2k@hotmail.com>
2025-07-17 03:29:42 +0900

    HeroSection_update_motion 에러 해결

--- explore-canada/src/App.jsx
+++ explore-canada/src/App.jsx
@@ -3,11 +3,13 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Header from './components/Header';
+import HeroSection from './components/sections/Hero/HeroSection';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <HeroSection />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- explore-canada/src/components/sections/Hero/HeroSection.jsx
+++ explore-canada/src/components/sections/Hero/HeroSection.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+import { motion } from 'framer-motion';
 import { FaArrowDown } from 'react-icons/fa';
 
 const HeroSection = () => {
